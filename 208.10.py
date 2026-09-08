293a1a4c2b9ba501d6fd040256aa352bd8226e02
kashif mubashir <kashifmubashir0905@gmail.com>
2026-09-08 11:23:07 +0530

    Add files via upload

---
+++ 208.10.py
@@ -0,0 +1,9 @@
+ch=input("enter a character:")
+if ch.isupper():
+ print("upper case")
+elif ch.islower():
+ print("lower case")
+elif ch.isdigit():
+ print("digit")
+else:
+ print("special character")
