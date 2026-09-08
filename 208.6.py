d72fe6e0951aa074049ca95ba796d15d1325c282
kashif mubashir <kashifmubashir0905@gmail.com>
2026-09-08 11:12:40 +0530

    Add files via upload

---
+++ 208.6.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print(a, "is the largest")
+elif b > a:
+    print(b, "is the largest")
+else:
+    print("Both numbers are equal")
