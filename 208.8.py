13aa96b90165b9b16fd5cd9e8745c67aaa980088
kashif mubashir <kashifmubashir0905@gmail.com>
2026-09-08 11:16:18 +0530

    Add files via upload

---
+++ 208.8.py
@@ -0,0 +1,8 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+c=int(input("enter third number:"))
+
+if a==b and b==c:
+  print("all three numbers are equal")
+else:
+  print("all three numbers are unequal")
